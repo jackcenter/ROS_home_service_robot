c5c76dd0cd9bb0053414e109e978d3fee6f273dd
ff <ff>
2020-07-08 22:22:08 +0000

    added initial pose info

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-xterm -e export ROBOT_INITIAL_POSE="-x 1 -y -2 -Y 3" &
+xterm -e "export ROBOT_INITIAL_POSE='-x 2 -y 1 -Y 3'; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/worlds/myWorld.world" &
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/worlds/myWorld.world x_pos:=1 y_pos:=-2 Y_pos:=3" &
-sleep 100
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/maps/map.yaml initial_pose_x:=1.0 initial_pose_y:=-2.0 initial_pose_a:=1.57" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
